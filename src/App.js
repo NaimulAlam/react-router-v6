0fd84c54f7fc9e3a8f28465e810c63c47f455e2d
Mohmammad Naimul Alam <naimul2707@gmail.com>
2023-01-22 14:17:33 +0100

    first- basic routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <div>Default page</div> },
+    { path: "home", element: <Home /> },
+    { path: "about", element: <About /> },
+    { path: "products", element: <Products /> },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <a href="/home">Home</a> <a href="/about">About</a>
+      <br />
+      <Link to="/home">Home</Link>{" "}<Link to="/about">About</Link>{" "}
+      <Link to="/products">Products</Link>
+    </div>
+  );
+};
+
+export default Header;
