80ef5be6b25ba42a7ef1f58283534a3ca3cc3757
Mohmammad Naimul Alam <naimul2707@gmail.com>
2023-01-24 23:48:42 +0100

    finished practice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/home">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/friends">Friends</Link>
-      <Link to="/posts">Posts</Link>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/home"
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/products">Products</NavLink>
+      <NavLink to="/friends">Friends</NavLink>
+      <NavLink to="/posts">Posts</NavLink>
     </div>
   );
 };
